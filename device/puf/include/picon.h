3ec343730a3a4c64a2319f6d1590597455876c0c
gaufille <>
2006-11-11 14:14:40 +0000

    Add PIC components library (contains only picon currently)

---
+++ device/puf/include/picon.h
@@ -0,0 +1,37 @@
+/*-------------------------------------------------------------------------
+  picon.h - USB endpoint 2 callbacks
+
+             (c) 2006 Pierre Gaufillet <pierre.gaufillet@magic.fr> 
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+-------------------------------------------------------------------------*/
+
+/* $Id$ */
+
+#ifndef PICON_H_
+#define PICON_H_
+
+// Size of the End point packets in bytes
+#define PICON_PACKET_SIZE 8
+
+// Size of the circular string buffer
+#define PICON_BUFFER_SIZE 64
+
+void picon_init(void);
+void picon_in(void);
+void prepare_in(void);
+void picon_puts(uchar* s);
+
+#endif /*PICON_H_*/

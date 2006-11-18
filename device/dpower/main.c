8733d3e6e226df8639e737ed60a1ba1849639a98
gaufille <>
2006-11-18 19:53:51 +0000

    Use now sequencer

--- device/dpower/main.c
+++ device/dpower/main.c
@@ -23,6 +23,8 @@
 #include "debug.h"
 #include "boot_iface.h"
 #include "power_mgr.h"
+#include "sequencer.h"
+#include "timer2.h"
 #include <stdio.h>
 
 #define ftoggle_A0() { PORTAbits.AN0 = !PORTAbits.AN0; }
@@ -33,22 +35,57 @@
 // beware : this is not a C main function, but the application
 // entry point called from the boot.
 
-const uchar picon_ep = 3; // picon EP
+// picon EP
+const uchar picon_ep = 3;
+
+// RT sequencer counter
+static volatile uint counter;
+// RT sequencer release point flag (raised when a period begins) 
+static volatile uchar seq_flag;
 
 void application_main(void) 
 {
     PORTAbits.AN0 = 1;
-    stdout = STREAM_USER;
+    
+    // Init picon
+    init_debug();
+    
+    // Switch USB management to interrupt
+    PIE2bits.USBIE = 1;
+
+    // Prepare Analogic input
     CMCON  = 0x07;
     TRISE  = 0x07;
-    PIE2bits.USBIE = 1; 
+    
+    // Program timer2 for 5ms period
+    timer2_set_period(250);
+    timer2_set_control(TIMER2_PRESCALER_16, TIMER2_POSTSCALER(15), TIMER2_ON);
+    PIR1bits.TMR2IF = 0;
+    PIE1bits.TMR2IE = 1;
+    
+    // Enable global and peripheral interrupts 
     INTCON = 0xc0;
+
+    // Init sequencer counter
+    counter = 0;
+    
     debug("Power supply manager initialized\n");  
 
     while(usb_active_cfg > 2)
     {
-        power_supply_mgr();
+        // Wait for event (timer2 or usb)
+        usb_sleep();
+        
+        // Release point reached ?
+        if(seq_flag)
+        {
+            seq_flag = 0;
+            rt_sequencer(counter);
+        }
     }
+    
+    // Interrupts are disabled in the bootloader
+    // So no worry
     PORTAbits.AN0 = 0;
 }
 
@@ -56,6 +93,7 @@ void application_main(void)
 #pragma code high_priority_isr 0x2020
 void high_priority_isr(void) interrupt
 {
+    // Process USB interrupts
     if(PIR2bits.USBIF)
     {
         dispatch_usb_event();
@@ -63,6 +101,15 @@ void high_priority_isr(void) interrupt
         UIRbits.URSTIF = 0;
         PIR2bits.USBIF = 0;
         UEIR = 0;
+    }
+    
+    // Process timer2 interrupts
+    if(PIR1bits.TMR2IF)
+    {
+        PIR1bits.TMR2IF = 0;
+        // Switch on interrupt tail and increment RT counter
+        seq_flag = 1;
+        counter++;
     }
 }
 

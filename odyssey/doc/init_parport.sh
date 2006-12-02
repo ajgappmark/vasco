0d1f8a4f1e47c9cc599a2b2cd70fb063077fe75e
gaufille <>
2006-12-02 21:01:48 +0000

    Lightly improved support for parallel port setting (ppdev)

---
+++ odyssey/doc/init_parport.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+# $Id$
+# This script should be executed as root
+
+# stop cups that claims the parallel port
+/etc/init.d/cupsys stop
+
+# unload the parallel printer module
+modprobe -r lp
+
+# load the user parallel port module
+modprobe ppdev
+
+echo Initialization done.
+echo Now, you can initialize the parallel programmer with odyssey init.

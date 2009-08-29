34ddb82f786a7d9c00efc1fea0f2b3e0f76bd444
gaufille <>
2009-08-29 20:22:13 +0000

    fixed cupsys that became cups in debian, added usbmon / sys/debug stuff

--- device/puf/scripts/init_host.sh
+++ device/puf/scripts/init_host.sh
@@ -1,17 +1,22 @@
 #/bin/sh
 # $Id$
 
-# stop cups that claims the parallel port
-/etc/init.d/cupsys stop
-# unload the parallel printer module
+# Stop cups that claims the parallel port
+/etc/init.d/cups stop
+# Unload the parallel printer module
 modprobe -r lp
-# load the user parallel port module
+# Load the user parallel port module
 modprobe ppdev
 
 TOOLSDIR=`dirname \`pwd\``/tools
 
-# give user read access to USB debug monitor
-chmod a+r /sys/kernel/debug/usbmon/2t
+# Load USB debugging module
+modprobe usbmon
+# Mount /sys debug filesystem
+mount -t debugfs none_debugs /sys/kernel/debug
+# Give user read access to USB debug monitor
+chmod a+r /sys/kernel/debug/usbmon/*
 
 echo Initialization done.
 echo Now, you can initialize the parallel programmer with $TOOLSDIR/bin/odyssey init.
+echo USB packets may be traced in /sys/kernel/debug/usbmon or using Wireshark.
